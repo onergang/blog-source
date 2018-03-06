d115f10a6317553bffaaf19ceaa3aa288f24d678
GaoGang <523871991@qq.com>
2018-03-06 16:53:22 +0800

    inital

--- replace.sh
+++ replace.sh
@@ -1,5 +1,5 @@
 # custom
-mv themes/jacman/_config.yml _config.yml.bak
-/bin/cp custome/_config.yml themes/jacman/_config.yml
-/bin/cp custome/*.jpg  themes/jacman/source/
-/bin/cp custome/*.ico  themes/jacman/source/
+mv ./themes/jacman/_config.yml _config.yml.bak
+/bin/cp custom/_config.yml themes/jacman/_config.yml
+/bin/cp custom/*.jpg  themes/jacman/source/
+/bin/cp custom/*.ico  themes/jacman/source/
